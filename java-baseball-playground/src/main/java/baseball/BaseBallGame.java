1564eaa90c57222eb21a88c3c1f3c2209ade175c
nyoongoon <nyoongoon@naver.com>
2024-02-13 07:09:21 +0900

    커밋

--- java-baseball-playground/src/main/java/baseball/BaseBallGame.java
+++ java-baseball-playground/src/main/java/baseball/BaseBallGame.java
@@ -4,43 +4,68 @@ import java.util.Scanner;
 
 public class BaseBallGame {
     Scanner scanner = new Scanner(System.in);
-    int number;
+    String number;
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
 
     public void setting() {
-        number = (int) (Math.random() * 899) + 100;
+        number = String.valueOf((int) (Math.random() * 899) + 100);
+    }
+
+    public boolean validateDigits(String input) {
+        int number = Integer.parseInt(input);
+        if (100 <= number && number <= 999) {
+            return true;
+        }
+        return false;
     }
 
     public void manualSetting() {
         System.out.println("숫자를 입력해주세요.");
         // 입력받기 &&  변환 가능 판단
         String input = scanner.nextLine();
-        number = transToNum(input, scanner);
-
-
-        // 변환
-
-        // 자리수
-
-
-//        while(number == null){
-//            System.out.println("숫자를 입력해주세요.");
-//        }
-//        while(str.length() != 3){
-//            System.out.println("올바른 숫자를 입력해주세요.");
-//            str = scanner.nextLine();
-//        }
-//        number = str;
+        this.number = getInputNumber(input);
     }
 
-    public int transToNum(String input, Scanner scanner) {
-        int result = 0;
+    public String getInputNumber(String input) {
+        String result = checkNumeric(input); //숫자인지 판단
+        return checkDigits(result); //자리수 판단
+    }
+
+    public String checkNumeric(String input){
+        String result = input;
+        // 변환
         try {
-            result = Integer.parseInt(input);
+            Integer.parseInt(input);
         } catch (NumberFormatException e) {
             System.out.println("잘못된 숫자입니다. 다시 입력해 주세요.");
-            String reInput
-            result = transToNum()
+            String reInput = this.scanner.nextLine();
+            result = getInputNumber(reInput);
         }
-        return null;
+        return result;
+    }
+
+    public String checkDigits(String input){
+        String result = input;
+        // 자리수
+        while (!validateDigits(this.number)) {
+            System.out.println("범위를 벗어났습니다. 다시 입력해주세요.");
+            result = getInputNumber(scanner.nextLine());
+        }
+        return result;
+    }
+
+
+
+    public void judge(String input){
+        int cnt = 0;
+        String ball = getInputNumber(input);
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+
     }
 }
--- java-baseball-playground/src/test/java/baseball/BaseBallGameTest.java
+++ java-baseball-playground/src/test/java/baseball/BaseBallGameTest.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BaseBallGameTest {
     BaseBallGame baseBallGame;
     OutputStream out;
+
     @BeforeEach
-    void init(){
+    void init() {
         baseBallGame = new BaseBallGame();
         baseBallGame.setting();
 
@@ -25,47 +26,64 @@ class BaseBallGameTest {
 
     @Test
     @DisplayName("숫자 랜덤 추가")
-    void test0(){
-        assertThat(baseBallGame.number.length()).isEqualTo(3);
+    void test0() {
+        baseBallGame.validateDigits(baseBallGame.number);
     }
 
     @Test
     @DisplayName("숫자 직접 추가")
-    void test1(){
+    void test1() {
         String input = "123";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-
+        Scanner scanner = new Scanner(System.in);
+        baseBallGame.setScanner(scanner);
+        baseBallGame.manualSetting();
+        assertThat(baseBallGame.number).isEqualTo(input);
     }
+
     @Test
-    @DisplayName("숫자 직접 추가 - 잘못 입력")
-    void test1_2(){
-        int result = Integer.parseInt("ㄱ");
+    @DisplayName("숫자 직접 추가 - 숫자가 아닌 문자 입력")
+    void test1_2() {
+        String input = "ㄱ\n" + "321\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        baseBallGame.setScanner(new Scanner(System.in));
+        baseBallGame.manualSetting();
+        assertThat(baseBallGame.number).isEqualTo("321");
     }
 
-    void input(){
-
+    @Test
+    @DisplayName("숫자 직접 추가 - 범위 벗어난 숫자 입력")
+    void test1_3() {
+        String input = "-1\n" + "1111\n" + "789";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        baseBallGame.setScanner(new Scanner(System.in));
+        baseBallGame.manualSetting();
+        assertThat(baseBallGame.number).isEqualTo("789");
     }
 
 
     @Test
     @DisplayName("모두 틀림")
-    void test2(){
+    void test2() {
 
     }
+
     @Test
     @DisplayName("하나 맞음")
-    void test3(){
+    void test3() {
 
     }
+
     @Test
     @DisplayName("두개 맞음")
-    void test4(){
+    void test4() {
 
     }
+
     @Test
     @DisplayName("다 맞음")
-    void test5(){
+    void test5() {
 
     }
 }
