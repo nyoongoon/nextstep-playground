05d312ac5ec393e144f20aea11512a7587e0fcd5
nyoongoon <nyoongoon@naver.com>
2024-04-01 08:01:14 +0900

    제이슨 강사 강의 정리

--- java-baseball-playground/src/main/java/json_teacher/domain/NumberGenerator.java
+++ java-baseball-playground/src/main/java/json_teacher/domain/NumberGenerator.java
@@ -1,9 +1,21 @@
 package json_teacher.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NumberGenerator {
-    public List<Integer> createRandomNumbers(){
+    public List<Integer> createRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
 
+        while (numbers.size() < 3) {
+            int number = new Random().nextInt(9) + 1;
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
+
+        }
+
+        return null;
     }
 }
--- java-baseball-playground/src/main/java/json_teacher/domain/Referee.java
+++ java-baseball-playground/src/main/java/json_teacher/domain/Referee.java
@@ -1,4 +1,24 @@
 package json_teacher.domain;
 
+import java.util.List;
+
 public class Referee {
+    public String compare(List<Integer> computer, List<Integer> player) {
+        final Jugment jugment = new Jugment();
+        int correctCount = jugment.correctCount(computer, player);
+
+        int strike = 0;
+        for (int placeIndex = 0; placeIndex < player.size(); placeIndex++) {
+            if (jugment.hasPlace(computer, placeIndex, player.get(placeIndex))) {
+                strike++;
+            }
+        }
+        int ball = correctCount - strike;
+
+        if(correctCount == 0){
+            return "아웃";
+        }
+
+        return ball + " 볼 " + strike + " 스트라이크";
+    }
 }
